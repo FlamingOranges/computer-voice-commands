818af238a525d2e7edebcab8788f0b1903ff66c6
ElijahG-ME <greige@mymacewan.ca>
2025-07-17 21:06:18 -0600

    this pisses me off

--- __main__.py
+++ __main__.py
@@ -3,8 +3,9 @@
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
 import time
-import os
+from commands import *
 import speech_recognition as sr
+
 
 
 # this is called from the background thread
@@ -12,11 +13,15 @@ def callback(recognizer, audio):
     # received audio data, now we'll recognize it using Google Speech Recognition
     try:
         
-        print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+        speech = recognizer.recognize_google(audio).lower()
+        print("Google Speech Recognition thinks you said " + speech)
+        if speech in commandsDict:
+            commandsDict[speech]()
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
 
 
 r = sr.Recognizer()
@@ -26,10 +31,11 @@ with m as source:
 
 # start listening in the background (note that we don't have to do this inside a `with` statement)
 stop_listening = r.listen_in_background(m, callback)
+print("Listening...")
 # `stop_listening` is now a function that, when called, stops background listening
 
 
 # calling this function requests that the background listener stop listening
 #stop_listening(wait_for_stop=False)
 
-def jackass():
+commandsDict = {"computer open jackass": jackass, "stop listening": stop_listening}
---
+++ commands.py
@@ -0,0 +1,4 @@
+import os
+
+def jackass():
+    os.system("jackass.url")
